863e86e01eee5e21bdb434247f415a0a3ddd9889
Chetan <chetan@kisan.net>
2019-01-03 18:40:37 +0530

    changes in browser.java

---
+++ KISANCHAT-25-04-2018/src/test/java/pageObjects/DiscoverPge.java
@@ -0,0 +1,11 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import utility.BaseClass;
+
+public class DiscoverPge extends BaseClass{
+
+	public DiscoverPge(WebDriver driver) {
+		super(driver);
+	}
+}
--- KISANCHAT-25-04-2018/src/test/java/utility/BrowserFactory.java
+++ KISANCHAT-25-04-2018/src/test/java/utility/BrowserFactory.java
@@ -2,14 +2,17 @@ package utility;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BrowserFactory{
 	public static WebDriver driver;
 	
 	public void chromeBrowser() {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--disable-notifications");
 		System.setProperty("webdriver.chrome.driver", "E:\\Chetan-Selenium\\git\\KISAN.NET_WEB_APP\\KISANCHAT-25-04-2018\\browser-drivers\\chromedriver.exe");
-		driver = new ChromeDriver();
+		driver = new ChromeDriver(options);
 		driver.manage().window().maximize();
 		
 //		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
