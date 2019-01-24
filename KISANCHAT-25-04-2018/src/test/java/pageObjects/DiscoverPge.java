c37958218900dd5e8f9d9bb0afeff003fac3a4bc
Chetan <chetan@kisan.net>
2019-01-24 10:16:12 +0530

    TC folder added

--- KISANCHAT-25-04-2018/src/test/java/pageObjects/DiscoverPge.java
+++ KISANCHAT-25-04-2018/src/test/java/pageObjects/DiscoverPge.java
@@ -1,11 +1,40 @@
 package pageObjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import utility.BaseClass;
+import utility.DataFile;
 
 public class DiscoverPge extends BaseClass{
 
 	public DiscoverPge(WebDriver driver) {
 		super(driver);
+	}
+	
+	By filterBtn = By.xpath("//*[@id=\"lightgrayColorBody\"]/app-root/app-discover-channels/mat-sidenav-container/mat-sidenav-content/div/div/mat-list/mat-list-item[2]/div/div[3]/div[2]/div");
+	By searchBtn = By.xpath("//*[@id=\"lightgrayColorBody\"]/app-root/app-discover-channels/mat-sidenav-container/mat-sidenav-content/div/div/mat-list/mat-list-item[2]/div/div[3]/div[1]/div/input");
+	By closeBtn = By.xpath("//*[@id=\"lightgrayColorBody\"]/app-root/app-discover-channels/mat-sidenav-container/mat-sidenav-content/div/div/mat-list/mat-list-item[1]/div/a");
+	By searchEdit = By.xpath("//*[@id=\"lightgrayColorBody\"]/app-root/app-discover-channels/mat-sidenav-container/mat-sidenav-content/div/div/mat-list/mat-list-item[2]/div/div[3]/div[1]/div/div/input");
+	By searchClose = By.xpath("//*[@id=\"lightgrayColorBody\"]/app-root/app-discover-channels/mat-sidenav-container/mat-sidenav-content/div/div/mat-list/mat-list-item[2]/div/div[3]/div[1]/div/div/div");
+	
+	DataFile input = new DataFile(driver);
+	
+	public void click_filterBtn() {
+		waitTillElementClickable(filterBtn);
+		driver.findElement(filterBtn).click();
+	}
+	
+	public void searchChnl() throws Exception {
+		//clicking on search button
+		waitTillElementClickable(searchBtn);
+		driver.findElement(searchBtn).click();
+		//type in search input 
+		waitTillElementPresent(searchEdit);
+		driver.findElement(searchEdit).click();
+		driver.findElement(searchEdit).clear();
+		//set the channel name in search input field from excel (data) file 
+		String chnlName = input.readTextInput("Channel", 3, 0);
+		setText(searchEdit, chnlName);
+		
 	}
 }
--- KISANCHAT-25-04-2018/src/test/java/testCases/LOGIN .NET/EXHIBITOR_LOGIN.java
+++ KISANCHAT-25-04-2018/src/test/java/testCases/LOGIN .NET/EXHIBITOR_LOGIN.java
@@ -1,4 +1,4 @@
-package testCases;
+package testCases.LOGIN .NET;
 
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
